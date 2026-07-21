40f600ae8d4a509420ed022f063689f5a8f42647
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-07-20 21:11:32 -0300

    Remove unnecessary precoTotal field
    Refactor the Ex06 class to calculate price on-demand instead of storing it as state. Remove the precoTotal instance variable and eliminate the getPreco() call from the constructor. The getPreco() method now returns the calculation directly, simplifying the code and removing unnecessary state management.

--- src/exercicios17072026parte1/Ex06.java
+++ src/exercicios17072026parte1/Ex06.java
@@ -5,19 +5,16 @@ public class Ex06 {
 
 	private static final double PRECO_HORA = 0.08;
 	private int quantidadePaginas = 0;
-	private double precoTotal = 0;
 	
 	public Ex06() { }
 
 	public Ex06(int quantidadePaginas) {		
-		this.quantidadePaginas = quantidadePaginas;
-		this.getPreco();
+		this.quantidadePaginas = quantidadePaginas;		
 	}
 
 	public double getPreco() {
 		// Lembrete: cada folha corresponde a duas páginas, frente e verso		
-		this.precoTotal = ((quantidadePaginas / 2) * PRECO_HORA);
-		return precoTotal;
+		return ((this.quantidadePaginas / 2) * PRECO_HORA);
 	}
 	
 	public void setQuantidadePaginas(int quantidadePaginas) {
@@ -31,7 +28,8 @@ public class Ex06 {
 	@Override
 	public String toString() {
 		return "Ex06 [quantidadePaginas=" + this.getQuantidadePaginas() + 
-				", precoTotal=" + this.getPreco() + "]";
+				", precoTotal=" + this.getPreco() + 
+				"]";
 	}
 		
 }

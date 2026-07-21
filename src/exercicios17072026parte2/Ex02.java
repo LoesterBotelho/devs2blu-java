6f5a9fbdfa62bf253c62b090fcac2b45be1ef0dc
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-07-20 21:18:18 -0300

    Compute area/perimeter on demand
    Remove cached area and perimetro fields from Ex02 and stop storing computed values. getArea() and getPerimetro() now return computed results directly; constructor and setter no longer call those methods. toString() reformatted. Simplifies class and prevents stale cached values while preserving behavior.

--- src/exercicios17072026parte2/Ex02.java
+++ src/exercicios17072026parte2/Ex02.java
@@ -3,15 +3,11 @@ package exercicios17072026parte2;
 public class Ex02 {
 
 	private double ladoQuadrado = 0.0;
-	private double area = 0.0;
-	private double perimetro = 0.0;
 	
 	public Ex02() {}
 
 	public Ex02(double ladoQuadrado) {
 		this.ladoQuadrado = ladoQuadrado;
-		getArea();
-		getPerimetro();
 	}
 
 	public double getLadoQuadrado() {
@@ -20,23 +16,22 @@ public class Ex02 {
 	
 	public void setLadoQuadrado(double ladoQuadrado) {
 		this.ladoQuadrado = ladoQuadrado;
-		getArea();
-		getPerimetro();
 	}
 	
 	public double getArea() {
-		this.area = this.ladoQuadrado * this.ladoQuadrado;
-		return this.area;
+		return this.ladoQuadrado * this.ladoQuadrado;
 	}
 	
 	public double getPerimetro() {
-		this.perimetro = this.ladoQuadrado * 4;
-		return this.perimetro;
+		return this.ladoQuadrado * 4;
 	}
 
 	@Override
 	public String toString() {
-		return "Ex02 [ladoQuadrado=" + this.getLadoQuadrado() + ", area=" + this.getArea() + ", perimetro=" + this.getPerimetro() + "]";
+		return "Ex02 [ladoQuadrado=" + this.getLadoQuadrado() + 
+				", area=" + this.getArea() + 
+				", perimetro=" + this.getPerimetro() + 
+				"]";
 	}
 			
 }

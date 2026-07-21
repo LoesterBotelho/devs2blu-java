c5894350617b845c84f7b0e533589247cde5d886
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-07-20 21:12:00 -0300

    Refactor Ex08: remove redundant fields and simplify getters
    Remove unused `minutos` and `segundos` instance fields since these are derived values computed from `horas`. Simplify the getter methods to compute and return values directly instead of storing them, reducing unnecessary state and improving code clarity.

--- src/exercicios17072026parte1/Ex08.java
+++ src/exercicios17072026parte1/Ex08.java
@@ -3,15 +3,11 @@ package exercicios17072026parte1;
 public class Ex08 {
 
 	private int horas = 0;
-	private int minutos = 0;
-	private int segundos = 0;
 
 	public Ex08() { }
 
 	public Ex08(int horas) {
 		this.horas = horas;
-		this.getMinutos();
-		this.getSegundos();
 	}
 
 	public int getHoras() {
@@ -23,13 +19,11 @@ public class Ex08 {
 	}
 
 	public int getMinutos() {
-		this.minutos = this.horas * 60;
-		return this.minutos;
+		return this.horas * 60;
 	}
 
 	public int getSegundos() {
-		this.segundos = this.horas * 3600;
-		return this.segundos;
+		return this.horas * 3600;
 	}
 
 	@Override

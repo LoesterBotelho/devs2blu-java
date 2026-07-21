a65f2c49718b47ee82754f753a6220f3cde08167
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-07-20 21:11:10 -0300

    Refactor Ex05: remove unnecessary state field
    Remove the precoTotal instance field and its initialization. The getPrecoTotal() method now computes and returns the price directly without storing intermediate state. Also modernize local variable declarations using var keyword.

--- src/exercicios17072026parte1/Ex05.java
+++ src/exercicios17072026parte1/Ex05.java
@@ -5,23 +5,19 @@ public class Ex05 {
 	private static final double PRECO_HORA = 2.30;
 	private static final double PRECO_MINUTO = 0.0383;
 	private int totalMinutos = 0;
-	private double precoTotal = 0;
 
 	public Ex05() { }
 
 	public Ex05(int totalMinutos) {
 		this.totalMinutos = totalMinutos;
-		this.getPrecoTotal();
 	}
 
 	public double getPrecoTotal() {
 		
-		int horas = (this.totalMinutos / 60);
-		int minutos = this.totalMinutos - (horas * 60);
+		var horas = (this.totalMinutos / 60);
+		var minutos = this.totalMinutos - (horas * 60);
 		
-		this.precoTotal = (horas * PRECO_HORA) + (minutos * PRECO_MINUTO);
-
-		return this.precoTotal;
+		return (horas * PRECO_HORA) + (minutos * PRECO_MINUTO);
 	}
 	
 	public void setTotalMinutos(int totalMinutos) {

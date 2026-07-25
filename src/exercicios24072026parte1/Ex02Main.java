da7d566af285f45d7ebb72edcca26a1c80363710
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-07-24 22:35:48 -0300

    estuda java

--- src/exercicios24072026parte1/Ex02Main.java
+++ src/exercicios24072026parte1/Ex02Main.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 
 public class Ex02Main {
 
-//	Criar um programa que leia os limites inferior e superior de um intervalo e 
-//	imprima todos os números pares no intervalo aberto e seu somatório. 
-//	Suponha que os números digitados são um intervalo crescente. Exemplo:
-//		Limite inferior: 3 Limite superior: 12 Saída: 4 6 8 10 Soma: 28
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
--- src/exercicios24072026parte1/Ex05Main.java
+++ src/exercicios24072026parte1/Ex05Main.java
@@ -4,30 +4,62 @@ import java.util.Scanner;
 
 public class Ex05Main {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		String nome = "";
-		int qtdMultas = 0;
-		double SomaMultas = 0.0;
-		
-		System.out.println("Digite Seu nome : ");
-		nome = sc.nextLine();
-				
-		System.out.println("Digite qtd de multas : ");
-		qtdMultas = sc.nextInt();
+    public static void main(String[] args) {
 
-		while (qtdMultas <= 0)
-			System.out.println("Digite valor da multa : ");
-			SomaMultas += sc.nextDouble();
-			qtdMultas--;
-		}
-		
-		System.out.println("\nNome : " + nome + "Total de multas : " + SomaMultas);
-		
-		sc.close();			
+        Scanner sc = new Scanner(System.in);
 
-}
-	
+        int numeroCarteira;
+        int qtdMultas;
+        int maiorQtdMultas = -1;
+        int carteiraMaiorMultas = 0;
+
+        double valorMulta;
+        double dividaMotorista;
+        double totalArrecadado = 0;
+
+        while (true) {
+
+            System.out.print("\nDigite 0 para encerrar");
+            System.out.print("Número da carteira : ");
+            numeroCarteira = sc.nextInt();
+
+            if (numeroCarteira == 0) {
+                break;
+            }
+
+            System.out.print("Quantidade de multas : ");
+            qtdMultas = sc.nextInt();
+
+            dividaMotorista = 0;
+
+            for (int i = 1; i <= qtdMultas; i++) {
+
+                System.out.print("Valor da multa " + i + ": R$ ");
+                valorMulta = sc.nextDouble();
+
+                dividaMotorista += valorMulta;
+            }
+
+            System.out.printf("Dívida do motorista: R$ %.2f%n", dividaMotorista);
+
+            totalArrecadado += dividaMotorista;
+
+            if (qtdMultas > maiorQtdMultas) {
+                maiorQtdMultas = qtdMultas;
+                carteiraMaiorMultas = numeroCarteira;
+            }
+        }
+
+        System.out.println("\n Saida : ");
+        System.out.printf("Total arrecadado: R$ %.2f%n", totalArrecadado);
+
+        if (maiorQtdMultas >= 0) {
+            System.out.println("Carteira com maior número de multas: " + carteiraMaiorMultas);
+            System.out.println("Quantidade de multas: " + maiorQtdMultas);
+        } else {
+            System.out.println("Nenhum motorista foi cadastrado.");
+        }
+
+        sc.close();
+    }
 }
